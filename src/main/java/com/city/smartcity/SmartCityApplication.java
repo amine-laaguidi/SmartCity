5b6aeb469d96b572be2897c41c5d1aedbb022549
algart19 <amine.laaguidi.9@gmail.com>
2023-05-23 18:11:56 +0100

    Switching to JBOSS(Undertow)

--- src/main/java/com/city/smartcity/SmartCityApplication.java
+++ src/main/java/com/city/smartcity/SmartCityApplication.java
@@ -1,12 +1,19 @@
 package com.city.smartcity;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SmartCityApplication {
+public class SmartCityApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SmartCityApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SmartCityApplication.class, args);
 	}
-
 }
